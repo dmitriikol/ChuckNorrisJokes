03b53afa102f6be6687c2d3ad291ccd150a5e32a
Dmitrii Kollegov <dmitriy.it@roxot.com>
2020-10-05 00:38:53 +0300

    feat: * progress dialog while page loading

--- app/src/main/java/com/example/chucknorrisjokes/Fragments/WebFragment.java
+++ app/src/main/java/com/example/chucknorrisjokes/Fragments/WebFragment.java
@@ -1,10 +1,14 @@
 package com.example.chucknorrisjokes.Fragments;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +21,7 @@ public class WebFragment extends Fragment {
 
     public static final String TITLE = "Api info";
     private final String URL = "http://www.icndb.com/api/";
+    ProgressDialog progressDialog;
 
     public static WebFragment getInstance() {
         WebFragment instance = new WebFragment();
@@ -34,7 +39,28 @@ public class WebFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        progressDialog = ProgressDialog.show(getActivity(), "", "Please wait...", true);
+
         WebView webView = (WebView) view.findViewById(R.id.web_view);
+
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getActivity(), description, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon)
+            {
+                progressDialog.show();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressDialog.dismiss();
+            }
+
+        });
+
         webView.loadUrl(URL);
     }
 }
